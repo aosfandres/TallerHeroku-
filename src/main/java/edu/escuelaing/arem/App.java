76a6f3a8bfc96c6e16cda9fc92c22c4878acc049
Andres sotelo <andres.sotelo@mail.escuelaing.edu.co>
2021-02-01 14:31:58 -0500

    prueba

--- src/main/java/edu/escuelaing/arem/App.java
+++ src/main/java/edu/escuelaing/arem/App.java
@@ -1,13 +1,20 @@
 package edu.escuelaing.arem;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+import static spark.Spark.*;
+
+
+
+public class App {
+
+	 public static void main(String[] args) {
+	 port(getPort());
+	 get("/hello", (req, res) -> "Hello Adres");
+	 }
+	 static int getPort() {
+	 if (System.getenv("PORT") != null) {
+	 return Integer.parseInt(System.getenv("PORT"));
+	 }
+	 return 4567; //returns default port if heroku-port isn't set( i.e. on localhost)
+	 }
 }
